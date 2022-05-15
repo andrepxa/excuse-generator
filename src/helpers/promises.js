c96843866a402ba9e36b8cd20c15279a17ce52d3
Andre Paxiega <andrepxa@gmail.com>
2022-05-14 23:45:03 -0300

    feat: add more promises

--- src/helpers/promises.js
+++ src/helpers/promises.js
@@ -1,6 +1,9 @@
 import { getRandom } from './getRandom'
 
-const PROMISES = ['vou beber unicamente cerveja']
+const PROMISES = [
+  'vou beber unicamente cerveja',
+  'ao meu eu do futuro que vai dar tudo certo'
+]
 
 export const getRandomPromise = () => {
   return getRandom(PROMISES)
