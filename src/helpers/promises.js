e2d737a9b549e7124e0fae95f07649f4de2b678a
Eric Santana <bitencourteric@hotmail.com>
2022-05-27 19:29:08 -0300

    feat: add a new impossible promise

--- src/helpers/promises.js
+++ src/helpers/promises.js
@@ -2,7 +2,8 @@ import { getRandom } from './getRandom'
 
 const PROMISES = [
   'vou beber unicamente cerveja',
-  'ao meu eu do futuro que vai dar tudo certo'
+  'ao meu eu do futuro que vai dar tudo certo',
+  'vou fazer um role light hoje'
 ]
 
 export const getRandomPromise = () => {
